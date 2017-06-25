678e700d5b583bd798bf0ce3a09e1e0648e447a1
melochin <hejunhuiok@hotmail.com>
2017-06-25 16:59:56 +0800

    增加Redis是否连接的判断

--- src/main/java/scratch/service/DictService.java
+++ src/main/java/scratch/service/DictService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.RedisConnectionFailureException;
+import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -23,13 +26,32 @@ public class DictService {
 	
 	private final static String REDIS_PREFIX = "dic_";
 	
+	private final static Logger log = Logger.getLogger(DictService.class);
+	
 	@SuppressWarnings("unchecked")
 	public Map<Long, String> findByType(String type) {
 		
-		if(redis.hasKey(REDIS_PREFIX + type)) {
+		// redis连接的状态标志
+		// 连接:数据将存储在缓存 	未连接:直接走DB
+		boolean connected = false;
+		
+		// 判断redis是否连接
+		try{
+			RedisConnection connection = redis.getConnectionFactory().getConnection();
+			connection.close();
+			connected = true;
+			log.debug("Redis连接，尝试从Redis读取数据");
+		} catch (RedisConnectionFailureException e) {
+			connected = false;
+			log.debug("Redis尚未连接，直接从数据库读取数据");
+		}
+		
+		// 尝试从redis中读取字典数据
+		if(connected && redis.hasKey(REDIS_PREFIX + type)) {
 			return  (Map<Long, String>) redis.opsForValue().get(REDIS_PREFIX + type);
 		}
 		
+		// 直接从DB中读取字典数据
 		List<Map<String, Object>> maps = dao.findByType(type);
 		if(maps == null || maps.size() == 0) {
 			return null;
@@ -39,7 +61,10 @@ public class DictService {
 			dics.put((Long)m.get("code"), (String)m.get("name"));
 		}
 		
-		redis.opsForValue().set(REDIS_PREFIX + type, dics, 6, TimeUnit.HOURS);
+		// 将数据缓存在redis中
+		if(connected) {
+			redis.opsForValue().set(REDIS_PREFIX + type, dics, 6, TimeUnit.HOURS);	
+		}
 		
 		return dics;
 	}
