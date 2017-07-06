08f2a709cb0a9c7c63471ac4159d6392955f9969
melochin <hejunhuiok@hotmail.com>
2017-07-06 10:57:03 +0800

    add MockMvc

--- src/test/java/scratch/test/ContextTest.java
+++ src/test/java/scratch/test/ContextTest.java
@@ -1,10 +1,15 @@
 package scratch.test;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -16,4 +21,13 @@ public class ContextTest {
 
 	@Autowired
 	protected WebApplicationContext wac;
+	
+	protected MockMvc mvc;
+	
+	@Before
+	public void setup() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(wac)
+				.alwaysExpect(status().isOk())
+				.build();
+	}
 }
