9820262e0f7cf677776b02b5f53c750e659dcc7a
melochin <hejunhuiok@hotmail.com>
2017-07-19 19:55:37 +0800

    修改文件工具类、网络访问工具类 1. （文件）新增直接根据URL下载文件的功能 2. （网络）新增设置header和request method的功能

--- src/main/java/scratch/support/FileUtils.java
+++ src/main/java/scratch/support/FileUtils.java
@@ -1,7 +1,82 @@
 package scratch.support;
 
-public class FileUtils {
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileUtils {
+	
+	public static void downloadFile(String url) throws IOException {
+		String[] words = url.split("/");
+		String fileName = null;
+		if(words.length > 1) {
+			String temp = words[words.length - 1];
+			if(temp.indexOf('.') >= 0) {
+				fileName = temp;
+			}
+		}
+		
+		if(fileName == null) {
+			fileName = String.valueOf(UUID.randomUUID());
+		}
+		
+		File file = new File(fileName);
+		downloadFile(url, file);
+	}
+	
+	/**
+	 * 将url的内容写到file里
+	 * @param url
+	 * @param file
+	 * @throws IOException 
+	 */
+	public static void downloadFile(String url, File file) throws IOException {
+		// 建立Connection
+		HttpURLConnection con = (HttpURLConnection)new URL(url).openConnection();
+		// 获取输入流
+		BufferedInputStream inputStream = new BufferedInputStream(con.getInputStream());
+		// 获取输出流
+		FileOutputStream writer = new FileOutputStream(file);
+		byte[] bytes = new byte[255];
+		int len = 0;
+		// 读取
+		while((len = inputStream.read(bytes, 0, 255))> 0) {
+			// 写出
+			writer.write(bytes, 0, len);	
+		}
+		writer.flush();
+		writer.close();
+	}
+	
+	
+	public static void uploadWithRandomName(MultipartFile multipartFile, HttpServletRequest request, String directory) {
+		// get file suffix
+		String originFilename = multipartFile.getOriginalFilename();
+		String suffix = FileUtils.getSuffix(originFilename);
+		// get file name
+		String filename = UUID.randomUUID().toString() + "." + suffix;
+		String path = request.getServletContext().getRealPath(directory);
+		String fullFileName = FilenameUtils.concat(path, filename);
+		// 保留文件后缀
+		File file = new File(fullFileName);
+		try {
+			multipartFile.transferTo(file);
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+
+	
 	public static String getSuffix(String fileName) {
 		String splits[] = fileName.split("\\.");
 		if(splits == null || splits.length < 2) {
--- src/main/java/scratch/support/web/HttpConnection.java
+++ src/main/java/scratch/support/web/HttpConnection.java
@@ -1,13 +1,24 @@
 package scratch.support.web;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import scratch.support.StringUtils;
 
 public class HttpConnection {
+	
+	private Map<String, String> params = new HashMap<String, String>();
+	private Map<String, String> headers = new HashMap<String, String>();
+	private String param = "";
+	
+	private String method = "get";
 	
 	public String connect(String url) {
 
@@ -22,6 +33,27 @@ public class HttpConnection {
 		return html;
 	}
 	
+	public HttpConnection method(String method) {
+		this.method = method;
+		return this;
+	}
+	
+	public HttpConnection param(String param) {
+		this.param = param;
+		return this;
+	}
+	
+	public HttpConnection param(String key, String value) {
+		params.put(key, value);
+		return this;
+	}
+	
+	public HttpConnection header(String key, String value) {
+		headers.put(key, value);
+		return this;
+	}
+	
+	
 	public String connect(URL url) {
 		String html = "";
 		HttpURLConnection connection = null;
@@ -30,7 +62,36 @@ public class HttpConnection {
 			connection.setRequestMethod("GET");
 			connection.setDoOutput(true);
 			connection.setRequestProperty("User-agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0");
+			
+			if("post".equals(method)) {
+				connection.setDoInput(true);
+				connection.setUseCaches(false);
+				connection.setRequestMethod("POST");
+			}
+			
+			for(Entry<String, String> entry : headers.entrySet()) {
+				connection.setRequestProperty(entry.getKey(), entry.getValue());	
+			}
+			
 			connection.connect();
+			
+			if("post".equals(method)) {
+				String result = "";
+				if(param.isEmpty()) {
+					StringBuilder param = new StringBuilder("");
+					for(Entry<String, String> entry : params.entrySet()) {
+						param.append(entry.getKey() + "=" + entry.getValue() + "&");
+					}
+					result = param.substring(0, param.length()-1).toString();	
+				} else {
+					result = param;
+				}
+				
+				OutputStream output = connection.getOutputStream();
+				output.write(result.getBytes("UTF-8"));
+				output.flush();	
+			}
+			
 			html = StringUtils.toString(connection.getInputStream(), "UTF-8");
 		} catch (IOException e) {
 			e.printStackTrace();
