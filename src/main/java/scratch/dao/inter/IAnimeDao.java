1e7fac9e819afe21219a1d0802b73302772b78f8
melochin <hejunhuiok@hotmail.com>
2017-08-10 20:31:04 +0800

    update anime service
    1.重构anime dao、service 代码
    2.对mybatis分页，新增拦截器适配PageBean数据类型

--- src/main/java/scratch/dao/inter/IAnimeDao.java
+++ src/main/java/scratch/dao/inter/IAnimeDao.java
@@ -4,28 +4,44 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
-import com.github.pagehelper.Page;
 import com.github.pagehelper.PageRowBounds;
 
 import scratch.model.Anime;
+import scratch.model.AnimeAlias;
+import scratch.support.service.PageBean;
 
 public interface IAnimeDao {
+	
+	/**---------------------------查询----------------------------------**/
+	
+	Anime getById(@Param("id") Long id);
+	
+	Anime getByAlias(@Param("alias") String alias, Boolean finished);
+	
+	/**关联查询：查询的anime带有alias数据**/
+	Anime getByIdWithAlias(@Param("id") Long id);
+	
+	/**查询所有的anime**/
+	List<Anime> list();
+	
+	/**条件查询:按类型和是否完结查询，如果参数为null,则不起条件限制作用**/
+	List<Anime> listIf(@Param("type") String type, @Param("finished") Boolean finished);
+	
+	/**关联查询：查询的anime带有alias数据**/
+	List<Anime> listWithAlias();
+	
+	/**关联查询：查询的anime，按focus数量排序**/
+	List<Anime> listMostFocused();
 
-	List<Anime> findAll();
+	/** 注意：由于排序中使用到了focus表，因此会进行表关联查询  **/
+	List<Anime> listByTypeLeftJointFocus(@Param("type") String type);
 	
-	List<Anime> findWithAlias();
-
-	List<Anime> findByType(@Param("type")String type);
+	List<Anime> listByNameLeftJoinFocus(@Param("name") String name);
 	
-	Page<Anime> find(@Param("type") String type, PageRowBounds page);
+	/**分页查询：根据类型，查询anime**/
+	PageBean<Anime> pageByType(@Param("type") String type, PageRowBounds page);
 	
-	List<Anime> findByFinished(@Param("finished") Boolean finished);
-	
-	List<Anime> findMostFocused();
-	
-	Anime findById(@Param("id") Long id);
-	
-	Anime findByAlias(@Param("alias") String alias, Boolean finished);
+	/**--------------------------更新--------------------------------------**/
 	
 	void save(@Param("anime") Anime anime);
 	
@@ -33,5 +49,12 @@ public interface IAnimeDao {
 	
 	void delete(@Param("id") Long id);
 
+	/**--------------------------更新Alias--------------------------------------**/
 	
+	void saveAlias(@Param("alias") AnimeAlias alias);
+	
+	void modifyAlias(@Param("alias") AnimeAlias alias);
+	
+	void deleteAlias(@Param("alias") AnimeAlias alias);
+
 }
--- src/main/java/scratch/service/AnimeService.java
+++ src/main/java/scratch/service/AnimeService.java
@@ -3,11 +3,15 @@ package scratch.service;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,48 +19,98 @@ import com.github.pagehelper.PageRowBounds;
 
 import scratch.dao.inter.IAnimeDao;
 import scratch.model.Anime;
+import scratch.model.AnimeAlias;
 import scratch.support.FileUtils;
 import scratch.support.PageFactory;
+import scratch.support.service.PageBean;
 
 @Service
 public class AnimeService {
 
+	/**
+	 * @deprecated 将用图片服务代替
+	 */
 	private static final String UPLOAD_ANIME = "/upload/anime";
 	
-	@Autowired
 	private IAnimeDao animeDao;
 	
-	public List<Anime> findMostFocus() {
-		return animeDao.findMostFocused();
+	@Autowired
+	public AnimeService(IAnimeDao animeDao) {
+		this.animeDao = animeDao;
 	}
 	
-	
-	public List<Anime> find(String type, int page) {
-		if(StringUtils.isEmpty(type)) {
-			type = null;
-		}
-		PageRowBounds pageRowBounds = null;
-		if(page > 0) {
-			pageRowBounds = PageFactory.asList(page);
-			return animeDao.find(type, pageRowBounds);	
-		}
-		return animeDao.findByType(type);
+	public List<Anime> list() {
+		return animeDao.list();
 	}
 	
-	public Anime findById(Long animeId) {
-		return animeDao.findById(animeId);
+	public List<Anime> listByType(String type) {
+		return animeDao.listByTypeLeftJointFocus(type);
 	}
 	
-	public List<Anime> findAll() {
-		return animeDao.findAll();
+	public List<Anime> listByName(String name) {
+		return animeDao.listByNameLeftJoinFocus(name);
 	}
 	
+	/** 查询最受关注的Anime **/
+	public List<Anime> listMostFocused() {
+		return animeDao.listMostFocused();
+	}
+	
+	/** 查询最受关注的Anime
+	 *	@param limit 限制显示个数
+	 */ 
+	public List<Anime> listMostFocused(int limit) {
+		List<Anime> animes = listMostFocused();
+		return animes.size() > limit ? animes.subList(0, limit) : animes;
+	}
+	
+	/** 将最受关注的Anime，根据类型分组 **/
+	public Map<String,List<Anime>> listMostFcousedGroupByType() {
+		List<Anime> animes = listMostFocused();
+		return animes.stream().collect(Collectors.groupingBy(Anime::getType));
+	}
+	
+	/**
+	 * 将最受关注的Anime，根据类型分组
+	 * @param limit 限制每组显示个数
+	 */
+	public Map<String,List<Anime>> listMostFcousedGroupByType(int limit) {
+		Map<String, List<Anime>> map = listMostFcousedGroupByType();
+		// 遍历分组的animes，对数量进行限制调整
+		map.entrySet().forEach(entry -> {
+			List<Anime> animes = entry.getValue();
+			animes = animes.size() > limit ? animes.subList(0, limit) : animes;
+			entry.setValue(animes);
+		});
+		return map;
+	}
+	
+	public PageBean<Anime> pageByType(String type, Integer page) {
+		// type 空字串 当 null处理
+		type = Optional.ofNullable(type)
+				.filter(s -> !StringUtils.isEmpty(s))
+				.orElse(null);
+		PageRowBounds pageRowBounds = PageFactory.asList(page);
+		return animeDao.pageByType(type, pageRowBounds);
+	}
+	
+	public Anime getById(Long animeId) {
+		return animeDao.getById(animeId);
+	}
+	
+	@Transactional
 	public void save(Anime anime) {
 		animeDao.save(anime);
 	}
 	
+	@Transactional
 	public void update(Anime anime) {
 		animeDao.update(anime);
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		animeDao.delete(id);
 	}
 	
 	/**
@@ -78,7 +132,7 @@ public class AnimeService {
 	public void updateWithFile(Anime anime, MultipartFile multipartFile, String realPath) 
 			throws IllegalStateException, IOException  {
 		//删除老的文件
-		Anime originAnime = animeDao.findById(anime.getId());
+		Anime originAnime = animeDao.getById(anime.getId());
 		deleteFile(originAnime.getPic(), realPath);
 		
 		//更新文件名
@@ -91,7 +145,7 @@ public class AnimeService {
 	}
 
 	public void deleteWithFile(Long animeId, String realPath) {
-		Anime anime = animeDao.findById(animeId);
+		Anime anime = animeDao.getById(animeId);
 		deleteFile(anime.getPic(), realPath);
 		animeDao.delete(animeId);
 	}
@@ -109,6 +163,39 @@ public class AnimeService {
 		if(!StringUtils.isEmpty(filename)) {
 			File file = new File(realPath + UPLOAD_ANIME, filename);
 			file.delete();
+		}
+	}
+
+	public Anime findByIdWithAlias(Long id) {
+		return animeDao.getByIdWithAlias(id);
+	}
+
+	@Transactional
+	public void saveAlias(AnimeAlias alias) {
+		animeDao.saveAlias(alias);
+	}
+
+	@Transactional
+	public void modifyAlias(AnimeAlias alias) {
+		animeDao.modifyAlias(alias);
+	}
+
+	@Transactional
+	public void deleteAlias(AnimeAlias alias) {
+		animeDao.deleteAlias(alias);
+	}
+	
+	/**
+	 * alias不存在时，执行新增
+	 * 反之更新
+	 * @param alias
+	 */
+	@Transactional
+	public void saveOrModifyAlias(AnimeAlias alias) {
+		try{
+			saveAlias(alias);	
+		} catch (Exception e) {
+			modifyAlias(alias);
 		}
 	}
 
