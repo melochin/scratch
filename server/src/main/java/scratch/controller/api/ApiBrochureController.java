7c9812ec98fff0bbaa5ee7148990af4b92d21499
Kaze <hejunhuiok@hotmail.com>
2018-04-08 10:20:33 +0800

    update card controller

--- server/src/main/java/scratch/controller/api/ApiBrochureController.java
+++ server/src/main/java/scratch/controller/api/ApiBrochureController.java
@@ -1,42 +1,41 @@
 package scratch.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 import scratch.dao.BrochureRepository;
-import scratch.model.RedisKey;
+import scratch.dao.CardRepository;
 import scratch.model.entity.Brochure;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @RestController
 public class ApiBrochureController {
 
 	@Autowired
-	private BrochureRepository repository;
+	private BrochureRepository brochureRepository;
+
+	@Autowired
+	private CardRepository cardRepository;
 
 	@GetMapping("/api/brochures")
 	public List<Brochure> list() {
-		return repository.list();
+		return brochureRepository.list();
 	}
 
 	@PostMapping("/api/brochures")
 	public void save(@RequestBody Brochure brochure) {
-		repository.save(brochure);
+		brochureRepository.save(brochure);
 	}
 
 	@PutMapping("/api/brochures")
 	public void modify(@RequestBody Brochure brochure) {
-		repository.modify(brochure);
+		brochureRepository.modify(brochure);
 	}
 
 	@DeleteMapping("/api/brochures/{brochureId}")
 	public void delete(@PathVariable("brochureId") String brochureId) {
-		repository.delete(brochureId);
+		brochureRepository.delete(brochureId);
 	}
 
 }
--- server/src/main/java/scratch/controller/api/ApiCardController.java
+++ server/src/main/java/scratch/controller/api/ApiCardController.java
@@ -1,38 +1,20 @@
 package scratch.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Scheduler;
-import reactor.util.function.Tuple2;
 import scratch.dao.BrochureRepository;
 import scratch.dao.CardRepository;
-import scratch.model.RedisKey;
-import scratch.model.entity.Brochure;
 import scratch.model.entity.Card;
-import scratch.service.Handler;
 import scratch.service.Listener;
 import scratch.service.ListenerService;
-import scratch.service.RedisService;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import java.time.Duration;
 import java.util.*;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 public class ApiCardController {
-
-	@Autowired
-	private RedisTemplate redisTemplate;
 
 	@Autowired
 	private CardRepository cardRepository;
@@ -57,20 +39,34 @@ public class ApiCardController {
 		return Flux.create(fluxSink -> {
 			fluxSink.next(cardRepository.list(brochureId));
 			listenerService.addListener(
-					new Listener<Set>(name, data -> fluxSink.next(data)));
+					new Listener<List>(name, data -> fluxSink.next(data)));
 		});
 	}
 
 	@GetMapping(value="/api/brochures/{brochureId}/cards")
-	public Map list(@PathVariable("brochureId") String brochureId) {
-		Set<Card> cards = cardRepository.list(brochureId);
-		Brochure brochure = brochureRepository.find(brochureId);
-
-		Map result = new HashMap();
-		result.put("brochure", brochure);
-		result.put("cards", cards);
-		return result;
+	public List<Card> list(@PathVariable("brochureId") String brochureId) {
+		return cardRepository.list(brochureId);
 	}
+
+	@GetMapping(value="/api/brochures/{brochureId}/cards/memory")
+	public List<Card> memory(@PathVariable("brochureId") String brochureId) {
+
+		Integer size = brochureRepository.findMemory(brochureId);
+		if(size == null) {
+			return cardRepository.list(brochureId);
+		}
+
+		return cardRepository.list(brochureId, size-1);
+	}
+
+	@PutMapping("/api/brochures/{brochureId}/cards/memory")
+	public void modifyMemory(@PathVariable("brochureId") String brochureId, @RequestBody Card card) {
+		Integer memory = Optional.ofNullable(brochureRepository.findMemory(brochureId))
+				.orElse(cardRepository.count(brochureId));
+		brochureRepository.modifyMemory(brochureId, memory-1);
+		cardRepository.deleteAndSave(brochureId, card);
+	}
+
 
 	@DeleteMapping("/api/brochures/{brochureId}/cards")
 	public void delete(@PathVariable("brochureId") String brochureId,
